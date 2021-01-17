9c14b8e65bac4cf4ba8c837dc2bd39fa2304eaca
Anshika Gangwar <45893300+AnshikaGangwar@users.noreply.github.com>
2021-01-17 14:58:39 +0530

    photo container

--- client/src/components/myPhoto.jsx
+++ client/src/components/myPhoto.jsx
@@ -1,25 +1,35 @@
 import React, { Component } from 'react'
 import Typist from 'react-typist'
+import Me from '../assets/me.svg'
 
 export default class MyPhoto extends Component {
-    mycursor={
-        color: "#E1DDDD",
-        show: true,
-        blink: true,
-        element: '|',
-        hideWhenDone: false,
-        hideWhenDoneDelay: 1000,
-    }
-
+    
     delay=120;
+    stdelay1=1400;
+    stdelay2=2500;
+    hideWhenDoneDelay=true;
     render() {
         return (
-            <div className="container-fluid body vh-100 w-100">
-            <Typist className="cursor" avgTypingDelay={this.delay}>
-                <p><span className="me_text">PASSIONATE.</span></p>
-                <p><span className="me_text">CREATIVE.</span></p>
-                <p><span className="me_text">VERSATILE.</span></p>
+            <div className="container-fluid me_body vh-100 w-100">
+            <div className="row container">
+            <div className="col-md-8" style={{zIndex:'2'}}>
+            {/* <Typist cursor={{show:false}} avgTypingDelay={this.delay} >
+                        <span className="me_text">PASSIONATE.</span> 
             </Typist>
+            <Typist cursor={{show:false}} onLineTyped={this.onlineTyped2} avgTypingDelay={this.delay} startDelay={this.stdelay1} >
+                        <span className="me_text">CREATIVE.</span> 
+            </Typist>
+            <Typist  cursor={{show:false}} onLineTyped={this.onlineTyped3} avgTypingDelay={this.delay} startDelay={this.stdelay2}>
+                        <span className="me_text">VERSATILE.</span> 
+            </Typist> */}
+            <div className="me_wrapper">
+            <span className="me_name">Anshika Gangwar</span>
+            </div>
+            </div>
+            <div className="col-md-4">
+              <img className="me_photo" src={Me}/>
+            </div>
+            </div>            
             </div>
         )
     }
--- client/src/components/skills.jsx
+++ client/src/components/skills.jsx
@@ -17,40 +17,40 @@ export default class Skill extends Component {
   render() {
     return (
       <div className="skill_body">
-      <div className=" container container-fluid ">
+      <div className=" container container-fluid">
          <span className="center skill_title">Snooping About Me</span>
-      <div className="skill_wrapper">
+      <div className="skill_wrapper ">
           <div className="row justify-content-center row-position">
               <div className="icon-text">
-              <img className="" src={this.state.icons[0]} width="20%"/>
+              <img className="rotate" src={this.state.icons[0]} width="20%"/>
               <span className="skill_text">{this.state.skills[0]}</span>
               </div>
           </div>
           <div className="row justify-content-center row-position">
                <div className="col-md-6 text-center icon-text">
-               <img src={this.state.icons[1]} width="10%"/>
+               <img className="rotate" src={this.state.icons[1]} width="10%"/>
                <span className="skill_text">{this.state.skills[1]}</span>
                </div>
                <div className="col-md-6 text-center icon-text">
-               <img src={this.state.icons[2]} width="10%"/>
+               <img className="rotate" src={this.state.icons[2]} width="10%"/>
                <span className="skill_text">{this.state.skills[2]}</span>
                </div>
           </div>
           <div className="row justify-content-center row-position">
              <div className="icon-text">
-              <img src={this.state.icons[3]}/>
+              <img className="rotate1"src={this.state.icons[3]}/>
               <span className="skill_text">{this.state.skills[3]}</span>
               </div>
           </div>
           <div className="row justify-content-center row-position">
                <div className="col-md-6 text-center icon-text">
-               <img className="icon" src={this.state.icons[4]} width="10%"/>
+               <img className="icon rotate1" src={this.state.icons[4]} width="10%"/>
                <span className="skill_text">{this.state.skills[4]}</span>
                </div>
                <div className="col-md-6 text-center icon-text">
 
                <span className="skill_text">{this.state.skills[5]}</span>
-               <img className="" src={this.state.icons[5]} width="10%"/>
+               <img className="rotate1" src={this.state.icons[5]} width="10%"/>
                </div>
           </div>
       </div>
