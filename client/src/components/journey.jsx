de0f9387b666b6506bf008d88d601b72af2d2427
Naman Patel <46571853+np453@users.noreply.github.com>
2021-01-25 23:37:02 +0530

    minor text animations

--- client/src/components/journey.jsx
+++ client/src/components/journey.jsx
@@ -10,7 +10,22 @@ export default class Journey extends Component {
         return (
             <div className="container-fluid j-wrapper vh-100">
                 <div>
-                 <span className="skill_title d-flex justify-content-center">My Journey</span>
+                 <span className="skill_title d-flex justify-content-center">
+                     
+                 <svg viewBox="0 0 960 300">
+                    <symbol id="s-text">
+                        <text text-anchor="middle" x="50%" y="80%">My Journey</text>
+                    </symbol>
+
+                    <g class = "g-ants">
+                        <use href="#s-text" class="text-copy" />
+                        <use href="#s-text" class="text-copy" />
+                        <use href="#s-text" class="text-copy" />
+                        <use href="#s-text" class="text-copy" />
+                        <use href="#s-text" class="text-copy" />
+                    </g>
+                </svg>
+                 </span>
                  </div>
                <div className=" front ">
                 <div className=" circle1 ">
