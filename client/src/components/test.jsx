d6926743b97bdf66e7bfdd780620d3738a1b6463
Naman Patel <46571853+np453@users.noreply.github.com>
2021-02-07 19:13:19 +0530

    test component

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -4,29 +4,45 @@ import "../../src/sass/components/test.scss";
 class test extends Component {
     render() {
         return (
-            <div className="test_container">
-                <div class="stage">
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
-                    <div class="layer"></div>
+            // <div className="test_container">
+            //     <div class="stage">
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //         <div class="layer"></div>
+            //     </div>
+            // </div>
+
+            <div class="flowerContain">
+                <div class="leafContain">
+                    <img src="https://www.prashantsani.com/Misc/codepen/l1.svg" alt="" class="leaf leaf1" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l2.svg" alt="" class="leaf leaf2" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l3.svg" alt="" class="leaf leaf3" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l4.svg" alt="" class="leaf leaf4" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l5.svg" alt="" class="leaf leaf5" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l6.svg" alt="" class="leaf leaf6" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l7.svg" alt="" class="leaf leaf7" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l8.svg" alt="" class="leaf leaf8" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l9.svg" alt="" class="leaf leaf9" />
+                    <img src="https://www.prashantsani.com/Misc/codepen/l10.svg" alt=""class="leaf leaf10" />
                 </div>
+                <img src="https://www.prashantsani.com/Misc/codepen/center.svg" alt="" class="center" />
             </div>
         );
     }
