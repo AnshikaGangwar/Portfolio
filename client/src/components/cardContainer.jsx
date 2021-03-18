c821df36e92976b6cc39d55c33a50f7ebd810405
Anshika Gangwar <45893300+AnshikaGangwar@users.noreply.github.com>
2021-03-18 22:28:07 +0530

    nodemailer setup

--- client/src/components/cardContainer.jsx
+++ client/src/components/cardContainer.jsx
@@ -22,12 +22,10 @@ export default class CardContainer extends Component {
                           <img src={oneml} className="item-image1"/>
                           </div>
                           <div className="col-md-8">
-                          <span className="heading1">ONEML</span>
-                         <p className="content1 text-center w-100">Website - edu-social
-                                platformfor machine learning algorithms visualization
-                                Users can visualize the machine learning algorithms like linear
-                                regression and know how the various formulas work and their
-                                affecting factors</p>                
+                          <a href="https://github.com/AnshikaGangwar/Find_The_Fund">
+                          <span className="heading1">FIND THE FUND</span>
+                          </a>
+                         <p className="content1 text-center w-100">A model to predict whether the given investment is successful or not.</p>                
                           </div>
 
                       </div>
@@ -38,7 +36,9 @@ export default class CardContainer extends Component {
                           <img src={textrec} className="item-image2"/>
                           </div>
                           <div className="col-md-8">
+                          <a href="https://github.com/AnshikaGangwar/TextRecognition">
                           <span className="heading1">TEXTVIAIMAGE</span>
+                          </a>
                          <p className="content1 text-center w-80">Cross platform app to extract text from static images and facility to
                            share and save the text .</p>                
                           </div>
@@ -51,7 +51,7 @@ export default class CardContainer extends Component {
                           <img src={oneml} className="item-image1"/>
                           </div>
                           <div className="col-md-8">
-                          <span className="heading1">ONEML</span>
+                          <a href="https://github.com/AnshikaGangwar/OneML"><span className="heading1">ONEML</span></a>
                          <p className="content1 text-center w-80">Website - edu-social
                                 platform for machine learning algorithms visualization
                                 Users can visualize the machine learning algorithms like linear
